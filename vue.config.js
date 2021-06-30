09b7044de851c6c28be3d99d6e2b9bed71358ce5
David Long <davdaarn@live.com>
2021-06-29 19:52:38 -0600

    pre tailwind

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,15 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+
 module.exports = {
-	// configureWebpack: {
-	// 	target: 'electron-renderer'
-	// },
+	configureWebpack: {
+		// target: 'electron-renderer',
+		devtool: 'source-map'
+	},
 	pluginOptions: {
 		electronBuilder: {
 			chainWebpackRendererProcess: (config) => {
-				config.resolve.alias.set('@', path.resolve(__dirname + '/src/renderer'));
+				config.resolve.alias.set('@', path.resolve(__dirname + '/src/renderer/'));
 			},
 			mainProcessFile: "src/main/index.ts",
 			rendererProcessFile: "src/renderer/main.ts",
