14f643ae125671846fa733c05033b9085bb92070
David Long <davdaarn@live.com>
2021-06-29 20:39:37 -0600

    basic setup

---
+++ src/renderer/views/AppScreen.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>stuff</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+
+export default defineComponent({
+	name: "AppScreen",
+	components: {
+
+	},
+});
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	purge: ['./index.html', './renderer/src/**/*.{vue,js,ts,jsx,tsx}'],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+}
